1464399c1920bf627944b02931e96e49b5c9e721
majun <zte_majun@126.com>
2017-02-27 15:41:01 -0800

    add cors filter

--- SearchEngine/src/main/java/com/uci/indexer/BookUrlRepository.java
+++ SearchEngine/src/main/java/com/uci/indexer/BookUrlRepository.java
@@ -8,7 +8,6 @@ import com.uci.utils.JsonUtils;
 import com.uci.utils.SysPathUtil;
 
 import javax.annotation.PostConstruct;
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
