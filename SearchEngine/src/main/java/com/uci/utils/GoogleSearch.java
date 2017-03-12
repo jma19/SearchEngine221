5c191eed9c894a84b06c4c2e972f706186315dcf
chang27 <chanl27@uci.edu>
2017-03-11 18:07:01 -0800

    Update GoogleSearch.java

--- SearchEngine/src/main/java/com/uci/utils/GoogleSearch.java
+++ SearchEngine/src/main/java/com/uci/utils/GoogleSearch.java
@@ -1,5 +1,5 @@
-import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.api.client.http.javanet.NetHttpTransport;
@@ -10,29 +10,43 @@ import com.google.api.services.customsearch.model.Search;
 
 public class Google {
 
+ 
+    public static List<String> GoogleSearch(String query) {
 
-    public static void main(String[] args) {
         Customsearch customsearch = new Customsearch(new NetHttpTransport(), new JacksonFactory(), null);
-        String YOUR_SEARCH_STRING_GOES_HERE = "machine learning";
+        String q = query;
+        List<String> tmp = null;
         try {
-            com.google.api.services.customsearch.Customsearch.Cse.List list = customsearch.cse().list(YOUR_SEARCH_STRING_GOES_HERE);
+            Long num = Long.valueOf(10);
+            com.google.api.services.customsearch.Customsearch.Cse.List list = customsearch.cse().list(q);
             list.setKey("AIzaSyBaEI0o8NdDcq1lsoaAvly-ptTo2VnyLRY");
             list.setCx("000871406312733320210:n0ht0gelmoc");
-            Search results = list.setSiteSearch("ics.uci.edu").execute();
-            List<Result> items = results.getItems();
-            for (Result res : items){
-                System.out.println(res.getLink());
-            }
-           
 
+            Search results = list.setSiteSearch("ics.uci.edu").setNum(num).execute();
+            List<Result> items = results.getItems();
+            tmp = new ArrayList<>();
+            for (Result res : items) {
+                tmp.add(res.getLink());
+            }
           
 
         } catch (IOException e) {
-         
-                 e.printStackTrace();
-            //      }
+          
+            e.printStackTrace();
+        
         }
 
 
+        return tmp;
     }
+
+    public static void main(String[] args){
+        String a = "computer games";
+        List<String> b = GoogleSearch(a);
+        for(String s: b){
+            System.out.println(s);
+        }
+    }
+
 }
+
